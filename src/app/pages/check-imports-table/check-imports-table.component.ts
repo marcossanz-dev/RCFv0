cef382213b91e54831f6f6e25f1cbadde499e9d9
dondifer <dondifer@gmail.com>
2021-05-06 15:48:16 +0200

    fix: imports

--- src/app/pages/check-imports-table/check-imports-table.component.ts
+++ src/app/pages/check-imports-table/check-imports-table.component.ts
@@ -1,11 +1,5 @@
 import { SelectionModel } from '@angular/cdk/collections';
-import {
-  AfterContentChecked,
-  AfterViewInit,
-  Component,
-  Input,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ResizeEvent } from 'angular-resizable-element';
